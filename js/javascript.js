ac6c8cdadb1948ef043a715756a7cf5a4365a84e
fhros <112174015+fhros@users.noreply.github.com>
2023-04-25 10:30:36 +0300

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,11 @@
+
+auto = {
+    "name": "Fhros mobile",
+    "model": "Model U",
+    "made": 2022,
+    "company": "Fhros",
+    "website": "https://github.com/fhros",
+    "horsepower": function() {
+        alert(this.name + " " + this.model + " has 1000 horse power")
+    }
+}
